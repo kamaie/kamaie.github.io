7712a285e7074b6f00ec1992384b9a74c6d135a7
kamaie <kamaie_christian@yahoo.com>
2020-11-10 14:00:35 -0600

    TEST

--- lesson6/js/windChill.js
+++ lesson6/js/windChill.js
@@ -1,9 +1,9 @@
 function windChill() {
-    var tempF = parseFloat(document.getElementById("currentTemp").innerText);
+    var temp = parseFloat(document.getElementById("currentTemp").innerText);
     var speed = parseFloat(document.getElementById("windSpeed").innerText);
 
     // Calculate the Wind Chill Factor
-    var windChillFactor = 35.74 + 0.6215 * tempF - 35.75 * Math.pow(speed, 0.16) + 0.4275 * tempF * Math.pow(speed, 0.16);
+    var windChillFactor = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
 
     // Round windChillFactor
     var digits = 1;
