f5c13d035f4fa4c5185c9daa31475fa030be489e
kamaie <kamaie_christian@yahoo.com>
2020-11-11 12:19:55 -0600

    test

--- lesson6/js/windChill.js
+++ lesson6/js/windChill.js
@@ -2,12 +2,14 @@ function windChill() {
     var temp = document.getElementById("currentTemp");
     var speed =document.getElementById("windSpeed");
 
-    var windChillFactor = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
+    /*var windChillFactor = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);*/
+
+    var windChillftr = temp * speed;
 
     // Round
     var digits = 1;
     var multiplier = Math.pow(10, digits);
     var windChillRounded = Math.round(windChillFactor * multiplier ) / multiplier;
 
-    document.getElementById("windChill").innerHTML = windChillFactor;
+    document.getElementById("windChill").innerHTML = windChillftr;
 }
