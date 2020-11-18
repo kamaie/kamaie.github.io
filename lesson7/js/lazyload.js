fdcaa10dffbd838167d04bafb5de34a149eb6e1d
kamaie <kamaie_christian@yahoo.com>
2020-11-18 13:22:26 -0600

    test

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -1,33 +1,27 @@
-const imagesToLoad = document.querySelectorAll("img[data-src]");
-
-
+let imagesToLoad = document.querySelectorAll('img[data-src]');
 
 const imgOptions = {
-  threshold: 0.5
+  threshold: 1,
+  rootMargin: "0px 0px 50px 0px"
 };
 
 const loadImages = (image) => {
   image.setAttribute('src', image.getAttribute('data-src'));
-  image.onload = () => {
-    image.removeAttribute("data-src");
-  };
+  image.onload = () => {image.removeAttribute('data-src');};
 };
 
-
-if ('IntersectionObserver' in window) {
-  const imgObserver = new IntersectionObserver((items, imgObserver) => {
+if('IntersectionObserver' in window) {
+  const observer = new IntersectionObserver((items, observer) => {
     items.forEach((item) => {
-      if (item.isIntersecting) {
-        console.log("Target", item.target)
+      if(item.isIntersecting) {
         loadImages(item.target);
-        imgObserver.unobserve(item.target);
+        observer.unobserve(item.target);
       }
     });
   }, imgOptions);
 
-
   imagesToLoad.forEach((img) => {
-    imgObserver.observe(img);
+    observer.observe(img);
   });
 } else {
   imagesToLoad.forEach((img) => {
